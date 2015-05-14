e7923f6b184b8da3b460eb4c989eff409e4ff7e9
suigin <suigin@national.shitposting.agency>
2015-05-13 22:33:43 -0700

    no need to link to libm, not using it

--- config.mk
+++ config.mk
@@ -10,7 +10,7 @@ X11LIB = /usr/X11R6/lib
 
 # includes and libs
 INCS = -I. -I/usr/include -I${X11INC}
-LIBS = -L/usr/lib -lc -lm -L${X11LIB} -lX11 -lEGL -lGLESv2
+LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 -lEGL -lGLESv2
 
 # toolchain flags
 CPPFLAGS = -DVERSION=\"${VERSION}\" -D_POSIX_C_SOURCE=200112L
